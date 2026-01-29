92e12b935fc25b2606fcbe8d1af7d01eb58385fc
chaseusejavascripts <chasedaddy4@gmail.com>
2026-01-29 10:25:14 -0500

    Refactor car quiz logic and questions

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
-# Mini Project — Decision Quiz
-#Student Name: 
-# Topic: If / Elif / Else
-# Goal: Create a quiz that gives different feedback based on the user's answer
+print("welcome to my car quiz") 
 
-print("Welcome to the Decision Quiz!")
-print("-----------------------------")
+print("what is my favorite brand")
 
-# TODO 1: Print your quiz question
-# Example: print("What is 2 + 2?")
-print("ENTER YOUR QUIZ QUESTION HERE")
+brand= input("what specific brand do I like the most")
 
-# TODO 2: Ask the user for their answer
-# Convert to int() only if you are comparing numbers
-answer = input("Enter your answer: ")
-
-# TODO 3: Create at least THREE possible outcomes using:
-# - if
-# - elif
-# - else
-#
-# Each outcome should print a different message
-# Example ideas:
-# - Correct answer
-# - Close answer
-# - Incorrect answer
-
-# Write your decision logic below
-
-
-
-# TODO 4: Print a closing message
-print("Thanks for taking the quiz!")
+if("Ford"):
+  print("Truck")
+elif("Dodge"):
+  print("Wrong")
+else:
+  print("trash car")
